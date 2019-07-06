bf596d08c56c74943470c83c059cb860911d8fa6
zzzssc <1556837456@qq.com>
2019-07-06 09:57:25 +0800

    Initial Commit

---
+++ 文件传输14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  文件传输14
+//
+//  Created by s20181106119 on 2019/7/6.
+//  Copyright © 2019 s20181106119. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
